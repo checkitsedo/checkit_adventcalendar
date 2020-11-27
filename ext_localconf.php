1f2732a613f70fe40873df38006d2be5c8bc29d3
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-11-27 15:06:17 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@
 /**
  * General ext_localconf file.
  */
-if (!defined('TYPO*_MODE')) {
+if (!\defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
@@ -11,12 +11,34 @@ if (!defined('TYPO*_MODE')) {
  * Configure Plugin
  */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Checkitsedo.checkit_adventcalendar',
+    'Checkit.checkit_adventcalendar',
     'Adventcalendar',
     [
-        'Adventcalendar' => 'show',
+        'Adventcalendar' => 'list',
     ],
     [
-        'Adventcalendar' => 'show',
+        'Adventcalendar' => 'list',
     ]
 );
+
+/**
+ * Register Icons
+ * Icon for plugin selection in content element will be loaded automatically
+ * if to be found in /Resources/Public/Icons/Extension.svg
+ * !! If you’re using SVG files, make sure they are 16×16 pixel in size. Even though SVG is scalable, TYPO3 9 won’t scale them down (whereas TYPO3 8 did)!
+ */
+if (TYPO3_MODE === 'BE') {
+    $ext = 'checkit_adventcalendar';
+    $icons = [
+        $ext . '-wizard-icon' => 'Extension.svg',
+    ];
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+        foreach ($icons AS $id => $filename) {
+            $iconRegistry->registerIcon(
+                $id,
+                $iconRegistry->detectIconProvider($filename),
+                ['source' => 'EXT:' . $ext . '/Resources/Public/Icons/' . $filename]
+            );
+        }
+    }
+}
