6d8d1c919384d311f6ae2032f12653d48c7c02df
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-11-29 22:38:37 +0100

    Update ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -16,3 +16,8 @@ if (!\defined('TYPO3_MODE')) {
     'Adventcalendar with Audio'
 );
 
+$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY); 
+$pluginSignature = strtolower($extensionName) . '_adventcalendar'; 
+
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY.'/Configuration/FlexForms/ControllerActions.xml'); 
